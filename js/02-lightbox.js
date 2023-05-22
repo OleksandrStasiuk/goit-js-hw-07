4625c44774e470a49006394409194719ed143b97
OleksandrStasiuk <122239424+OleksandrStasiuk@users.noreply.github.com>
2023-05-23 00:12:06 +0300

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+function createGalleryElements(items) {
+  let galleryElements = items.map((item) => {
+    let itemEl = document.createElement("li");
+    itemEl.classList.add("gallery__item");
+
+    let linkEl = document.createElement("a");
+    linkEl.classList.add("gallery__link");
+    linkEl.href = item.original;
+    itemEl.appendChild(linkEl);
+
+    let imgEl = document.createElement("img");
+    imgEl.classList.add("gallery__image");
+    imgEl.src = item.preview;
+    imgEl.alt = item.description;
+    linkEl.appendChild(imgEl);
+
+    return itemEl;
+  });
+
+  listEl.append(...galleryElements);
+  return listEl;
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+function bindEvents(galleryElements) {
+  listEl.addEventListener("click", onGalleryItemClick);
+}
+
+function initGallery(items) {
+  let galleryElements = createGalleryElements(items);
+  bindEvents(galleryElements);
+}
+initGallery(galleryItems);
